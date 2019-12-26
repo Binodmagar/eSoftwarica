6005d6873d5b42112dc50abb3b229cd657dc1122
Binodmagar <ranabinod615@gmail.com>
2019-12-26 21:18:03 +0545

    adding adapter packages where Student and StudentAdapter created

---
+++ app/src/main/java/com/binod/adapter/Student.java
@@ -0,0 +1,54 @@
+package com.binod.adapter;
+
+public class Student {
+
+    private String name, address, gender;
+    private int age, imgId;
+
+    public Student(String name, String address, String gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+}
---
+++ app/src/main/java/com/binod/adapter/StudentAdapter.java
@@ -0,0 +1,90 @@
+package com.binod.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.binod.esoftwarica.R;
+
+import java.util.List;
+
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+    Context context;
+    List<Student> studentList;
+
+    public StudentAdapter(Context context, List<Student> studentList) {
+        this.context = context;
+        this.studentList = studentList;
+    }
+
+    @NonNull
+    @Override
+    public StudentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.student_layout, parent, false);
+        return new StudentViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StudentViewHolder holder, final int position) {
+
+        Student student = studentList.get(position);
+        holder.tvName.setText(student.getName());
+        holder.tvAddress.setText(student.getAddress());
+        holder.tvGender.setText(student.getGender());
+        holder.tvAge.setText(Integer.toString(student.getAge()));
+        holder.imgEdit.setImageDrawable(context.getResources().getDrawable(R.drawable.edit));
+        holder.imgDelete.setImageDrawable(context.getResources().getDrawable(R.drawable.delete));
+
+        String gender = student.getGender();
+        if(gender == "Male"){
+            holder.imgProfile.setImageResource(R.drawable.male);
+        }else if(gender == "Female"){
+            holder.imgProfile.setImageResource(R.drawable.female);
+        }else if(gender == "Other"){
+            holder.imgProfile.setImageResource(R.drawable.other);
+        }
+
+        holder.imgDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Student studentdel = studentList.get(position);
+                studentList.remove(position);
+                notifyItemRemoved(position);
+                Toast.makeText(context, studentdel + " is removed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+
+    public class StudentViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvName, tvAge, tvAddress, tvGender;
+        ImageView imgProfile, imgEdit, imgDelete;
+
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvName = itemView.findViewById(R.id.tvName);
+            tvAge = itemView.findViewById(R.id.tvAge);
+            tvAddress = itemView.findViewById(R.id.tvAddress);
+            tvGender = itemView.findViewById(R.id.tvGender);
+            imgDelete = itemView.findViewById(R.id.imgDelete);
+            imgEdit = itemView.findViewById(R.id.imgEdit);
+            imgProfile = itemView.findViewById(R.id.imgProfile);
+        }
+    }
+
+}
