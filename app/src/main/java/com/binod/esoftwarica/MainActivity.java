f22b74a02cddcc62b2623152f805d2b7f6acd01e
Binodmagar <ranabinod615@gmail.com>
2019-12-26 23:21:32 +0545

    small changes in mainactivity.java

--- app/src/main/java/com/binod/esoftwarica/MainActivity.java
+++ app/src/main/java/com/binod/esoftwarica/MainActivity.java
@@ -1,14 +1,54 @@
 package com.binod.esoftwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.binod.adapter.Student;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static List<Student> studentLists = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        if(studentLists.isEmpty()){
+            studentLists.add(new Student("Alisha", "Japan", "Female", 19));
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,
+                new HomeFragment()).commit();
+
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.nav_home :
+                selectedFragment = new HomeFragment();
+                break;
+
+                case R.id.add_student :
+                    selectedFragment = new StudentFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,
+                    selectedFragment).commit();
+            return true;
+        }
+    };
 }
