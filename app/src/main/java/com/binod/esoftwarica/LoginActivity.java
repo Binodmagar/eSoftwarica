45d5fd195ebe782084b04a1268f5c5647992649a
Binodmagar <ranabinod615@gmail.com>
2019-12-26 17:06:06 +0545

    login design and code done

---
+++ app/src/main/java/com/binod/esoftwarica/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.binod.esoftwarica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword;
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkValidation();
+                if(etUsername.getText().toString().equals("softwarica") && etPassword.getText().toString().equals("softwarica")){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+    }
+    private void checkValidation(){
+
+        if(TextUtils.isEmpty(etUsername.getText())){
+            Toast.makeText(getApplicationContext(), "Please enter username", Toast.LENGTH_SHORT).show();
+        }else if (TextUtils.isEmpty(etPassword.getText())){
+            Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
